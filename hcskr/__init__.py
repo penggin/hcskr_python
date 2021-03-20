ce5a1d8cf67f65e43068d015bc8e881d00b3faeb
fxrcha <truetype24@gmail.com>
2021-03-21 01:23:11 +0900

    fix: __init__.py

--- hcskr/__init__.py
+++ hcskr/__init__.py
@@ -1,1 +1,14 @@
+from .hcs import *
+
 __version__ = "1.9.3"
+
+__all__ = [
+    "asyncSelfCehck",
+    "asyncUserLogin",
+    "asyncTokenSelfCheck",
+    "asyncGenerateToken",
+    "selfcheck",
+    "userlogin",
+    "tokenselfcheck",
+    "generatetoken",
+]
