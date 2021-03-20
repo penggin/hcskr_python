df96da3d81ee3c628c5d0f1d6a9b5940773bf23c
fxrcha <truetype24@gmail.com>
2021-03-21 01:19:17 +0900

    타입 체킹, 코드 간결화

--- hcskr/hcs.py
+++ hcskr/hcs.py
@@ -1,221 +1,214 @@
 import asyncio
-import base64
-import inspect
 import sys
+
+import jwt
+
 from base64 import b64decode, b64encode
 
-import aiohttp
-import jwt  # This module is "PyJWT" https://pypi.org/project/PyJWT/
-from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
-from Crypto.PublicKey import RSA
-
-from .mapping import schoolinfo
-
-versioninfo = "1.9.2"
-
-pubkey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA81dCnCKt0NVH7j5Oh2+SGgEU0aqi5u6sYXemouJWXOlZO3jqDsHYM1qfEjVvCOmeoMNFXYSXdNhflU7mjWP8jWUmkYIQ8o3FGqMzsMTNxr+bAp0cULWu9eYmycjJwWIxxB7vUwvpEUNicgW7v5nCwmF5HS33Hmn7yDzcfjfBs99K5xJEppHG0qc+q3YXxxPpwZNIRFn0Wtxt0Muh1U8avvWyw03uQ/wMBnzhwUC8T4G5NclLEWzOQExbQ4oDlZBv8BM/WxxuOyu0I8bDUDdutJOfREYRZBlazFHvRKNNQQD2qDfjRz484uFs7b5nykjaMB9k/EJAuHjJzGs9MMMWtQIDAQAB=="
-
-
-def encrypt(n):
-    rsa_public_key = b64decode(pubkey)
-    pub_key = RSA.importKey(rsa_public_key)
-
-    cipher = Cipher_pkcs1_v1_5.new(pub_key)
-    msg = n.encode("utf-8")
-
-    default_encrypt_length = 245
-    length = default_encrypt_length
-    msg_list = [msg[i : i + length] for i in list(range(0, len(msg), length))]
-    encrypt_msg_list = []
-    for msg_str in msg_list:
-        cipher_text = base64.b64encode(cipher.encrypt(message=msg_str))
-        encrypt_msg_list.append(cipher_text)
-    return encrypt_msg_list[0].decode("utf-8")
+from .mapping import schoolinfo, encrypt, pubkey
+from .request import send_hcsreq, search_school
 
 
 def selfcheck(
-    name,
-    birth,
-    area,
-    schoolname,
-    level,
-    password,
-    customloginname=None,
-    loop=asyncio.get_event_loop(),
+    name: str,
+    birth: str,
+    area: str,
+    schoolname: str,
+    level: str,
+    password: str,
+    customloginname: str = None,
 ):
-    return loop.run_until_complete(
-        asyncSelfCheck(name, birth, area, schoolname, level, password, customloginname)
-    )
+    return asyncio.get_event_loop().run_until_complete(asyncSelfCheck(**locals()))
 
 
 def userlogin(
-    name, birth, area, schoolname, level, password, loop=asyncio.get_event_loop()
+    name: str, birth: str, area: str, schoolname: str, level: str, password: str
 ):
-    return loop.run_until_complete(
-        asyncUserLogin(name, birth, area, schoolname, level, password)
-    )
+    return asyncio.get_event_loop().run_until_complete(asyncUserLogin(**locals()))
 
 
 def generatetoken(
-    name, birth, area, schoolname, level, password, loop=asyncio.get_event_loop()
+    name: str, birth: str, area: str, schoolname: str, level: str, password: str
 ):
-    return loop.run_until_complete(
-        asyncGenerateToken(name, birth, area, schoolname, level, password)
-    )
+    return asyncio.get_event_loop().run_until_complete(asyncGenerateToken(**locals()))
 
 
-def tokenselfcheck(token, loop=asyncio.get_event_loop()):
-    return loop.run_until_complete(asyncTokenSelfCheck(token))
+def tokenselfcheck(token: str):
+    return asyncio.get_event_loop().run_until_complete(asyncTokenSelfCheck(token))
 
 
 async def asyncSelfCheck(
-    name, birth, area, schoolname, level, password, customloginname=None
+    name: str,
+    birth: str,
+    area: str,
+    schoolname: str,
+    level: str,
+    password: str,
+    customloginname: str = None,
 ):
-    if customloginname == None:
+    if customloginname is None:
         customloginname = name
+
     login_result = await asyncUserLogin(name, birth, area, schoolname, level, password)
+
     if login_result["error"]:
         return login_result
-    token = login_result["token"]
-    info = login_result["info"]
-    schoolcode = login_result["schoolcode"]
-    async with aiohttp.ClientSession() as session:
-        # Hcs getUserInfo Request
-        endpoint = f"https://{info['schoolurl']}hcs.eduro.go.kr/v2/getUserInfo"
-        headers = {"Content-Type": "application/json", "Authorization": token}
-        data = {"orgCode": schoolcode}
-        async with session.post(endpoint, json=data, headers=headers) as response:
-            try:
-                res = await response.json()
-                token = res["token"]
-            except:
-                return {
-                    "error": True,
-                    "code": "UNKNOWN",
-                    "message": "getUserInfo: 알 수 없는 에러 발생.",
-                }
-        # Servey Register
-        endpoint = f"https://{info['schoolurl']}hcs.eduro.go.kr/registerServey"
-        headers = {"Content-Type": "application/json", "Authorization": token}
-        surveydata = {
-            "rspns01": "1",
-            "rspns02": "1",
-            "rspns00": "Y",
-            "upperToken": token,
-            "upperUserNameEncpt": customloginname,
+
+    try:
+        res = await send_hcsreq(
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": login_result["token"],
+            },
+            endpoint="/v2/getUserInfo",
+            school=login_result["info"]["schoolurl"],
+            json={"orgCode": login_result["schoolcode"]},
+        )
+
+        token = res["token"]
+
+    except Exception:
+        return {
+            "error": True,
+            "code": "UNKNOWN",
+            "message": "getUserInfo: 알 수 없는 에러 발생.",
         }
 
-        async with session.post(endpoint, json=surveydata, headers=headers) as response:
-            res = await response.json()
-            try:
-                return {
-                    "error": False,
-                    "code": "SUCCESS",
-                    "message": "성공적으로 자가진단을 수행하였습니다.",
-                    "regtime": res["registerDtm"],
-                }
-            except:
-                return {"error": True, "code": "UNKNOWN", "message": "알 수 없는 에러 발생."}
+    try:
+        res = await send_hcsreq(
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": token,
+            },
+            endpoint="/registerServey",
+            school=login_result["info"]["schoolurl"],
+            json={
+                "rspns01": "1",
+                "rspns02": "1",
+                "rspns00": "Y",
+                "upperToken": token,
+                "upperUserNameEncpt": customloginname,
+            },
+        )
+
+        return {
+            "error": False,
+            "code": "SUCCESS",
+            "message": "성공적으로 자가진단을 수행하였습니다.",
+            "regtime": res["registerDtm"],
+        }
+
+    except Exception:
+        return {"error": True, "code": "UNKNOWN", "message": "알 수 없는 에러 발생."}
 
 
-async def asyncUserLogin(name, birth, area, schoolname, level, password):
+async def asyncUserLogin(
+    name: str, birth: str, area: str, schoolname: str, level: str, password: str
+):
     name = encrypt(name)  # Encrypt Name
     birth = encrypt(birth)  # Encrypt Birth
     password = encrypt(password)  # Encrypt Password
+
     try:
         info = schoolinfo(area, level)  # Get schoolInfo from Hcs API
-    except:
+
+    except Exception:
         return {"error": True, "code": "FORMET", "message": "지역명이나 학교급을 잘못 입력하였습니다."}
-    url = f"https://hcs.eduro.go.kr/v2/searchSchool?lctnScCode={info['schoolcode']}&schulCrseScCode={info['schoollevel']}&orgName={schoolname}&loginType=school"
 
-    # REST Client open
-    async with aiohttp.ClientSession() as session:
-        # Get Request to given Url
-        async with session.get(url) as response:
-            school_infos = await response.json()
+    school_infos = await search_school(
+        code=info["schoolcode"], level=info["schoollevel"], org=schoolname
+    )
 
-            if len(school_infos["schulList"]) > 5:
-                return {
-                    "error": True,
-                    "code": "NOSCHOOL",
-                    "message": "너무 많은 학교가 검색되었습니다. 지역, 학교급을 제대로 입력하고 학교 이름을 보다 상세하게 적어주세요.",
-                }
-
-            try:
-                schoolcode = school_infos["schulList"][0]["orgCode"]
-            except:
-                return {
-                    "error": True,
-                    "code": "NOSCHOOL",
-                    "message": "검색 가능한 학교가 없습니다. 지역, 학교급을 제대로 입력하였는지 확인해주세요.",
-                }
-
-        # Trying Login Session for get auth token
-
-        data = {
-            "orgCode": schoolcode,
-            "name": name,
-            "birthday": birth,
-            "loginType": "school",
-            "stdntPNo": None,
+    if len(school_infos["schulList"]) > 5:
+        return {
+            "error": True,
+            "code": "NOSCHOOL",
+            "message": "너무 많은 학교가 검색되었습니다. 지역, 학교급을 제대로 입력하고 학교 이름을 보다 상세하게 적어주세요.",
         }
-        endpoint = f"https://{info['schoolurl']}hcs.eduro.go.kr/v2/findUser"
 
-        async with session.post(endpoint, json=data) as response:
-            res = await response.json()
-            try:
-                token = res["token"]
-            except:
-                return {
-                    "error": True,
-                    "code": "NOSTUDENT",
-                    "message": "학교는 검색하였으나, 입력한 정보의 학생을 찾을 수 없습니다.",
-                }
+    try:
+        schoolcode = school_infos["schulList"][0]["orgCode"]
 
-        # Hcs Password vaildtion
-        headers = {"Content-Type": "application/json", "Authorization": token}
-        data = {"password": password, "deviceUuid": ""}
-        endpoint = f"https://{info['schoolurl']}hcs.eduro.go.kr/v2/validatePassword"
+    except Exception:
+        return {
+            "error": True,
+            "code": "NOSCHOOL",
+            "message": "검색 가능한 학교가 없습니다. 지역, 학교급을 제대로 입력하였는지 확인해주세요.",
+        }
 
-        async with session.post(endpoint, json=data, headers=headers) as response:
+    try:
+        res = await send_hcsreq(
+            endpoint="/v2/findUser",
+            school=info["schoolurl"],
+            json={
+                "orgCode": schoolcode,
+                "name": name,
+                "birthday": birth,
+                "loginType": "school",
+                "stdntPNo": None,
+            },
+        )
 
-            try:
-                res = await response.json()
-                try:
-                    if res["isError"]:
-                        return {
-                            "error": True,
-                            "code": "PASSWORD",
-                            "message": "학생정보는 검색하였으나, 비밀번호가 틀립니다.",
-                        }
-                except:
-                    token = res
-            except:
-                return {
-                    "error": True,
-                    "code": "UNKNOWN",
-                    "message": "validatePassword: 알 수 없는 에러 발생.",
-                }
-        try:
-            caller_name = str(sys._getframe(1).f_code.co_name)
-        except:
-            caller_name = None
-        if caller_name == "asyncSelfCheck":
+        token = res["token"]
+
+    except Exception:
+        return {
+            "error": True,
+            "code": "NOSTUDENT",
+            "message": "학교는 검색하였으나, 입력한 정보의 학생을 찾을 수 없습니다.",
+        }
+
+    try:
+        res = await send_hcsreq(
+            headers={"Content-Type": "application/json", "Authorization": token},
+            endpoint="/v2/validatePassword",
+            school=info["schoolurl"],
+            json={"password": password, "deviceUuid": ""},
+        )
+
+        if res["isError"]:
             return {
-                "error": False,
-                "code": "SUCCESS",
-                "message": "유저 로그인 성공!",
-                "token": token,
-                "info": info,
-                "schoolcode": schoolcode,
+                "error": True,
+                "code": "PASSWORD",
+                "message": "학생정보는 검색하였으나, 비밀번호가 틀립니다.",
             }
-        return {"error": False, "code": "SUCCESS", "message": "유저 로그인 성공!"}
+
+        token = res
+
+    except Exception:
+        return {
+            "error": True,
+            "code": "UNKNOWN",
+            "message": "validatePassword: 알 수 없는 에러 발생.",
+        }
+
+    try:
+        caller_name = str(sys._getframe(1).f_code.co_name)
+
+    except Exception:
+        caller_name = None
+
+    if caller_name == "asyncSelfCheck":
+        return {
+            "error": False,
+            "code": "SUCCESS",
+            "message": "유저 로그인 성공!",
+            "token": token,
+            "info": info,
+            "schoolcode": schoolcode,
+        }
+
+    return {"error": False, "code": "SUCCESS", "message": "유저 로그인 성공!"}
 
 
-async def asyncGenerateToken(name, birth, area, schoolname, level, password):
-    login_result = await asyncUserLogin(name, birth, area, schoolname, level, password)
+async def asyncGenerateToken(
+    name: str, birth: str, area: str, schoolname: str, level: str, password: str
+):
+    login_result = await asyncUserLogin(**locals())
+
     if login_result["error"]:
         return login_result
+
     data = {
         "name": str(name),
         "birth": str(birth),
@@ -224,10 +217,14 @@ async def asyncGenerateToken(name, birth, area, schoolname, level, password):
         "level": str(level),
         "password": str(password),
     }
+
     jwt_token = jwt.encode(data, pubkey, algorithm="HS256")
+
     if isinstance(jwt_token, str):
         jwt_token = jwt_token.encode("utf8")
+
     token = b64encode(jwt_token).decode("utf8")
+
     return {
         "error": False,
         "code": "SUCCESS",
@@ -236,11 +233,13 @@ async def asyncGenerateToken(name, birth, area, schoolname, level, password):
     }
 
 
-async def asyncTokenSelfCheck(token, customloginname=None):
+async def asyncTokenSelfCheck(token: str, customloginname: str = None):
     try:
         data = jwt.decode(b64decode(token), pubkey, algorithms="HS256")
-    except Exception as e:
+
+    except Exception:
         return {"error": True, "code": "WRONGTOKEN", "message": "올바르지 않은 토큰입니다."}
+
     return await asyncSelfCheck(
         data["name"],
         data["birth"],
---
+++ hcskr/request.py
@@ -0,0 +1,19 @@
+from typing import Dict
+import aiohttp
+
+
+async def send_hcsreq(headers: Dict, endpoint: str, school: str, json: Dict):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            headers=headers, url=f"https://{school}hcs.eduro.go.kr{endpoint}", json=json
+        ) as resp:
+
+            return await resp.json()
+
+
+async def search_school(code: str, level: str, org: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=f"https://hcs.eduro.go.kr/v2/searchSchool?lctnScCode={code}&schulCrseScCode={level}&orgName={org}&loginType=school"
+        ) as resp:
+            return await resp.json()
